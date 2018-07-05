aa31472e138863cf239abc4f192722ce6c4f802c
ics-ikeda <ikeda@ics-web.jp>
2018-07-05 23:26:29 +0900

    ステップ1のJSを脱TypeScript化

--- docs/js/step1_2.js
+++ docs/js/step1_2.js
@@ -1,53 +1,30 @@
-var demo;
-(function (demo) {
-    var FileUpload = (function () {
-        function FileUpload() {
-            this._fileZone = document.getElementById("fileZone");
-            this._fileZoneInner = document.getElementById("fileZoneInner");
-            this.eventSetting();
-        }
-        /** 各種イベント設定 */
-        FileUpload.prototype.eventSetting = function () {
-            var _this = this;
-            // ドラッグアンドドロップイベント登録
-            this._fileZone.addEventListener("dragover", function (event) {
-                return _this.dragEnterHandler(event);
-            });
-            this._fileZone.addEventListener("dragleave", function (event) {
-                return _this.dragLeaveHandler(event);
-            });
-            this._fileZone.addEventListener("drop", function (event) {
-                return _this.dropHandler(event);
-            });
-        };
+// ファイルアップロードゾーン
+const fileZone = document.getElementById("fileZone");
+// ファイルアップロードゾーン中身
+const fileZoneInner = document.getElementById("fileZoneInner");
+// ドラッグアンドドロップイベント登録
+fileZone.addEventListener("dragover", dragEnterHandler);
+fileZone.addEventListener("dragleave", dragLeaveHandler);
+fileZone.addEventListener("drop", dropHandler);
 
-        /** ドラッグオーバー時の処理 */
-        FileUpload.prototype.dragEnterHandler = function (event) {
-            // デフォルトの挙動を停止
-            event.preventDefault();
-            this._fileZoneInner.className = "dragenter";
-        };
+/** ドラッグオーバー時の処理 */
+function dragEnterHandler(event) {
+  // デフォルトの挙動を停止
+  event.preventDefault();
+  fileZoneInner.className = "dragenter";
+}
 
-        /** ドラッグリーブ時のの処理 */
-        FileUpload.prototype.dragLeaveHandler = function (event) {
-            // デフォルトの挙動を停止
-            event.preventDefault();
-            this._fileZoneInner.className = "";
-        };
+/** ドラッグリーブ時のの処理 */
+function dragLeaveHandler(event) {
+  // デフォルトの挙動を停止
+  event.preventDefault();
+  fileZoneInner.className = "";
+}
 
-        /** ファイルをドロップした時の処理 */
-        FileUpload.prototype.dropHandler = function (event) {
-            // デフォルトの挙動を停止
-            event.preventDefault();
-            alert("読み込んだファイルは" + event.dataTransfer.files.toString());
-            this._fileZoneInner.className = "";
-        };
-        return FileUpload;
-    })();
-    demo.FileUpload = FileUpload;
-})(demo || (demo = {}));
-
-window.addEventListener("load", function () {
-    new demo.FileUpload();
-});
-//# sourceMappingURL=step1_2.js.map
+/** ファイルをドロップした時の処理 */
+function dropHandler(event) {
+  // デフォルトの挙動を停止
+  event.preventDefault();
+  alert("読み込んだファイルは" + event.dataTransfer.files.toString());
+  fileZoneInner.className = "";
+}
