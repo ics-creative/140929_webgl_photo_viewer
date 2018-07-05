c5e4c662bd240751f7c8b8034485666f2752c081
ics-ikeda <ikeda@ics-web.jp>
2018-07-05 23:45:58 +0900

    STEP4を脱TypeScript化

--- docs/js/step4.js
+++ docs/js/step4.js
@@ -1,33 +1,18 @@
-var demo;
-(function (demo) {
-    var DeviceOrientationTest = (function () {
-        function DeviceOrientationTest() {
-            this._consoleText = document.getElementById("consoleText");
-            this.eventSetting();
-        }
-        /** 各種イベント設定 */
-        DeviceOrientationTest.prototype.eventSetting = function () {
-            var _this = this;
-            window.addEventListener("deviceorientation", function (event) {
-                return _this.onRotationChange(event);
-            });
-        };
+const consoleText = document.getElementById("consoleText");
+window.addEventListener("deviceorientation", onRotationChange);
 
-        /** デバイスの傾きが変わった時 */
-        DeviceOrientationTest.prototype.onRotationChange = function (event) {
-            var alpha = Math.floor(event.alpha);
-            var beta = Math.floor(event.beta);
-            var gamma = Math.floor(event.gamma);
-            var compassHeading = Math.floor(event.webkitCompassHeading);
+/** デバイスの傾きが変わった時 */
+function onRotationChange(event) {
+  const alpha = Math.floor(event.alpha); // z軸の傾き
+  const beta = Math.floor(event.beta); // x軸の傾き
+  const gamma = Math.floor(event.gamma); // y軸の傾き
 
-            this._consoleText.innerHTML = "z軸の傾き alpha : " + alpha + "<br>" + "方位 CompassHeading : " + compassHeading + "<br>" + "x軸の傾き beta : " + beta + "<br>" + "y軸の傾き gamma : " + gamma;
-        };
-        return DeviceOrientationTest;
-    })();
-    demo.DeviceOrientationTest = DeviceOrientationTest;
-})(demo || (demo = {}));
+  // 方位の取得(Webkit限定)
+  const compassHeading = Math.floor(event.webkitCompassHeading);
 
-window.addEventListener("load", function () {
-    new demo.DeviceOrientationTest();
-});
-//# sourceMappingURL=step4.js.map
+  this.consoleText.innerHTML =
+    "z軸の傾き alpha : " + alpha + "<br/>" +
+    "方位 CompassHeading : " + compassHeading + "<br />" +
+    "x軸の傾き beta : " + beta + "<br/>" +
+    "y軸の傾き gamma : " + gamma;
+}
